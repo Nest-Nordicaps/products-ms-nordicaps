93db36dc6eb6e97b84b83f9e30cadf7113cf265c
Marcelo Mastroiani <mastroianimarcelo04@gmail.com>
2025-08-16 17:51:41 -0300

    refactor(products): update MessagePattern syntax for consistency

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -4,36 +4,37 @@ import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 
+//TODO: Se probo y funciona corectamente con el proxy de NATS
 @Controller()
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   // Crear un producto
-  @MessagePattern('create_product')
+  @MessagePattern({ cmd: 'create_product' })
   create(@Payload() createProductDto: CreateProductDto) {
     return this.productsService.create(createProductDto);
   }
 
   // Todos los productos
-  @MessagePattern('find_all_products')
+  @MessagePattern({ cmd: 'find_all_products' })
   findAll() {
     return this.productsService.findAll();
   }
 
   // Buscar un producto por ID
-  @MessagePattern('find_product')
+  @MessagePattern({ cmd: 'find_product' })
   findOne(@Payload('id', ParseIntPipe) id: number) {
     return this.productsService.findOne(id);
   }
 
   // Actualizar un producto
-  @MessagePattern('update_product')
+  @MessagePattern({ cmd: 'update_product' })
   update(@Payload() updateProductDto: UpdateProductDto) {
     return this.productsService.update(updateProductDto.id, updateProductDto);
   }
 
   // Eliminar un producto
-  @MessagePattern('remove_product')
+  @MessagePattern({ cmd: 'remove_product' })
   remove(@Payload('id', ParseIntPipe) id: number) {
     return this.productsService.remove(id);
   }
