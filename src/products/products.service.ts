e9516e5aa01912eb6fc979de644861528c555e72
Marcelo Mastroiani <mastroianimarcelo04@gmail.com>
2025-09-02 03:40:04 -0300

    refactor: update configuration files and improve imports for consistency

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
-import { PrismaClient } from 'generated/prisma';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class ProductsService extends PrismaClient implements OnModuleInit {
